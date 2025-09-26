ecc9c7494c507b0bde88ee83590af1435154c4f0
7011K <7011business@gmail.com>
2025-09-26 16:14:04 +0900

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,3 +1,4 @@
 // auth.js
 
+
 const GOOGLE_CLIENT_ID="341073342979-gfrpfpg3ag766tadn9rjnckrn7gd28sp.apps.googleusercontent.com";export let userEmail="";export function setupGoogleLogin(a){if(!window.google||!window.google.accounts){const b=document.createElement("script");b.src="https://accounts.google.com/gsi/client",b.async=!0,b.defer=!0,document.head.appendChild(b),b.onload=()=>{initLogin(a)}}else initLogin(a)}function initLogin(a){google.accounts.id.initialize({client_id:GOOGLE_CLIENT_ID,callback:b=>{const c=b.credential,d=JSON.parse(atob(c.split(".")[1]));userEmail=d.email?d.email.toLowerCase():"","function"==typeof a&&a(userEmail)}}),google.accounts.id.renderButton(document.getElementById("googleSignInBtn"),{theme:"outline",size:"large"})}
